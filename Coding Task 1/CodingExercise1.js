877ccbae763364114fa9371b0b980fd2a0fa701a
Todd Traylor <toddt67878@yahoo.com>
2020-03-30 12:22:24 -0500

    fourth commit

--- Coding Task 1/CodingExercise1.js
+++ Coding Task 1/CodingExercise1.js
@@ -16,7 +16,7 @@
 
 // SOLUTION:
 
-/*
+
 var scoreOfJohn = 80;
 // var scoreOfJohn = 49;
 var scoreOfNick = 45;
@@ -24,16 +24,15 @@ var scoreOfNick = 45;
 var pass = 51;
 
 if (scoreOfJohn >= pass && scoreOfNick >= pass) {
-  console.log('Both students passed');
-} else if(scoreOfJohn >= pass || scoreOfNick >= pass) {
-  console.log('One of the students passed:')
+    console.log('Both students passed');
+} else if (scoreOfJohn >= pass || scoreOfNick >= pass) {
+    console.log('One of the students passed:')
 
-   if (scoreOfJohn > scoreOfNick) {
-     console.log('and it is John with ' + scoreOfJohn + ' points');
-   } else {
-     console.log('and it is Nick with ' + scoreOfNick + ' points');
-   }
+    if (scoreOfJohn > scoreOfNick) {
+        console.log('and it is John with ' + scoreOfJohn + ' points');
+    } else {
+        console.log('and it is Nick with ' + scoreOfNick + ' points');
+    }
 } else {
-  console.log('Both of the students failed');
+    console.log('Both of the students failed');
 }
-*/
